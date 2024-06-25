0df0b095ba8593a46fd3819e469eb7a42e29853a
DamStell <82368599+DamStell@users.noreply.github.com>
2024-06-25 11:00:45 +0200

    Add void QuitGame

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,5 +8,9 @@ public class GameManager : MonoBehaviour
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
+     public void QuitGame()
+    {
+        Application.Quit();
+    }
 
 }
